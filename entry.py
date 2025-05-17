6c58d74b0925e6b6fa259229ebb012ff84c87183
jirawatteangthong <kodji0814814809@gmail.com>
2025-05-17 08:25:22 +0700

    Update strategy.py

--- entry.py
+++ entry.py
@@ -1,9 +1,13 @@
-from utils import fetch_current_price
+import talib
+import numpy as np
 
-def check_fibo_entry(fibo):
-    price = fetch_current_price()
-    if fibo['direction'] == 'long' and price <= fibo['entry']:
-        return fibo
-    elif fibo['direction'] == 'short' and price >= fibo['entry']:
-        return fibo
+def check_ema_cross(candles):
+    closes = [c[4] for c in candles]
+    ema50 = talib.EMA(np.array(closes), timeperiod=50)
+    ema200 = talib.EMA(np.array(closes), timeperiod=200)
+
+    if ema50[-2] < ema200[-2] and ema50[-1] > ema200[-1]:
+        return {'direction': 'long', 'price': closes[-1]}
+    elif ema50[-2] > ema200[-2] and ema50[-1] < ema200[-1]:
+        return {'direction': 'short', 'price': closes[-1]}
     return None
