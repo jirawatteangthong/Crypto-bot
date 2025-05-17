a896ce634d37b3ed0e7311d30db498c7c66ad5d7
jirawatteangthong <kodji0814814809@gmail.com>
2025-05-17 08:40:24 +0700

    Update telegram.py

--- telegram.py
+++ telegram.py
@@ -1,10 +1,15 @@
 import requests
-from config import TELEGRAM_TOKEN, TELEGRAM_CHAT_ID
 
-def send_message(msg):
-    url = f"https://api.telegram.org/bot{TELEGRAM_TOKEN}/sendMessage"
-    payload = {"chat_id": TELEGRAM_CHAT_ID, "text": msg}
-    requests.post(url, json=payload)
+BOT_TOKEN = '7752789264:AAF-0zdgHsSSYe7PS17ePYThOFP3k7AjxBY'
+CHAT_ID = '8104629569'
 
-def notify(msg):
-    send_message(msg)
+def send_telegram(message):
+    url = f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage"
+    payload = {
+        'chat_id': CHAT_ID,
+        'text': message
+    }
+    try:
+        requests.get(url, params=payload)
+    except Exception as e:
+        print(f"[ERROR TELEGRAM] {e}")
