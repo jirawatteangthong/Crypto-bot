a4df6817dfe751b6ee53ac9deb44974ba19a5ef3
jirawatteangthong <kodji0814814809@gmail.com>
2025-05-17 09:00:29 +0700

    Update and rename order.py to Config.py

---
+++ Config.py
@@ -0,0 +1,13 @@
+# --- ตั้งค่า API/บัญชี ---
+api_key = '8f528085-448c-4480-a2b0-d7f72afb38ad'
+secret = '05A665CEAF8B2161483DF63CB10085D2'
+password = 'Jirawat1-'
+
+symbol = 'BTC/USDT:USDT'
+leverage = 20
+position_size = 0.5  # ปรับง่าย
+telegram_token = '7752789264:AAF-0zdgHsSSYe7PS17ePYThOFP3k7AjxBY'
+telegram_chat_id = '8104629569'
+
+cooldown_after_sl_minutes = 5
+check_balance_every_hours = 5
